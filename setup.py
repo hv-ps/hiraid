36f9fa4fb762909ebebce8aee8020fe44be77c37
Darren Chambers <darren.chambers@hitachivantara.com>
2022-07-04 22:18:14 +0100

    Updated

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md', 'r') as fh:
 
 setuptools.setup(
     name='hiraid',
-    version='1.0.4',
+    version='1.0.5',
     description='Hitachi raidcom wrapper',
     long_description=long_description,
     long_description_content_type='text/markdown',
